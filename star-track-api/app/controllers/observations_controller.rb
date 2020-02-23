72143e0039656103bdde7cc67f1e39732546ef8e
Brittany Javalera <brittjavalera@gmail.com>
2020-02-23 12:33:34 -0800

    added render json to my destroy method

--- star-track-api/app/controllers/observations_controller.rb
+++ star-track-api/app/controllers/observations_controller.rb
@@ -14,6 +14,7 @@ class ObservationsController < ApplicationController
     def destroy
         observation = Observation.find_by_id(params[:id])
         observation.delete
+        render json: {observationID: observation.id}
     end
 
     private
