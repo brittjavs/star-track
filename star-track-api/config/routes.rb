cdf5921ec2510bf4d7c8257959c0eaab0f88fc71
Brittany Javalera <brittjavalera@gmail.com>
2020-02-26 20:45:20 -0800

    revised config/routes to include only the routes needed

--- star-track-api/config/routes.rb
+++ star-track-api/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :constellations
-  resources :observations
+  resources :constellations, only: [:index]
+  resources :observations, only: [:index, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
